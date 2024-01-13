d2062eb2310f352056dd22f7e65945c3dd7d1644
Jonah Kopp <jonah.kopp123@gmail.com>
2024-01-13 12:14:45 -0700

    completed bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -7,30 +7,53 @@ console.log("Data Promise: ", dataPromise);
 // Fetch the JSON data
 d3.json(url).then(function(data) {
 
-    // get the top 10 sample IDs and reverse them for cleaner plotting later
-    let labels = data.samples[0].otu_ids.slice(0,10).reverse()
+    // get the sample IDs
+    let labels = data.samples[0].otu_ids
 
-    // need to add 'OTU' so that plotly doesn't treat samples as integers in plotting
-    labels = labels.map(item => `OTU ${item}`)
+    // need to add 'OTU' so that plotly doesn't treat samples as integers
+    // in plotting in the bar chart
+    barLabels = labels.map(item => `OTU ${item}`)
 
-    values = data.samples[0].sample_values.slice(0,10).reverse()
+    // get sample values
+    let values = data.samples[0].sample_values
 
-    hoverLabels = data.samples[0].otu_labels.slice(0,10).reverse()
+    // get sample names
+    let hoverLabels = data.samples[0].otu_labels
     
     trace1 = {
-        x:values,
-        y:labels,
+        x:values.slice(0,10).reverse(),
+        y:barLabels.slice(0,10).reverse(),
         type:'bar',
         orientation:'h',
-        text:hoverLabels
+        text:hoverLabels.slice(0,10).reverse()
     }
 
     // Apply a title to the layout
-    let layout = {
+    let layout1 = {
         title:"Top 10 OTUs"
     }
 
     // Render the plot to the div tag with id "bar" on the page
-    Plotly.newPlot("bar", [trace1], layout);
+    Plotly.newPlot("bar", [trace1], layout1);
+
+    trace2 = {
+        x:labels,
+        y:values,
+        mode:'markers',
+        marker: {color:labels,
+                 size:values},
+        text:hoverLabels
+    }
+
+    let layout2 = {
+        title:"Bubble Chart"
+    }
+
+    // Render the plot to the div tag with id "bubble" on the page
+    Plotly.newPlot("bubble",[trace2],layout2)
+
+
+
+    
 
 });
